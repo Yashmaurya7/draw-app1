40bc299660466ee056076de87bca677868415ee6
YashMaurya7 <mauryayash2011@gmail.com>
2025-01-21 22:50:39 +0530

    added middleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,3 +1,17 @@
  import express from "express";
+import { JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken"  
  const app=express();
+ app.post("/signup",(req,res)=>{
+
+ })
+ app.post("/signin",(req,res)=>{
+    const userId=1;
+    const token=jwt.sign({
+        userId
+    },JWT_SECRET)
+     res.json({
+        token
+     })
+ })
  app.listen(3001)
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken"
+export function middleware(req: Request,res:Response,next:NextFunction){
+    const token=req.headers["authorization"]??"";
+    const decoded=jwt.verify(token,JWT_SECRET) as {userId:string};
+    if(decoded){
+     req.userId=decoded.userId;
+     next();
+    }else{
+        res.status(403).json({
+            message:"unauthorized"
+        })
+    }
+
+}
