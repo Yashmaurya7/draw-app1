7c95a27dc4ac3a292c8910fe02722dd7126acecf
YashMaurya7 <mauryayash2011@gmail.com>
2025-01-24 00:10:52 +0530

    added routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,19 +2,41 @@
 import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from "jsonwebtoken"  
 import { middleware } from "./middleware";
-import {CreateUser} from "@repo/common/types"
+import {CreateUser,SigninSchema,CreateRoomSchema} from "@repo/common/types"
+import {prismaClient} from "@repo/db/client"
  const app=express();
- app.post("/signup",(req,res)=>{
-    const data=CreateUser.safeParse(req.body);
-    if(!data.success){
+ app.post("/signup",async (req,res)=>{
+    const parsedData=CreateUser.safeParse(req.body);
+
+    if(!parsedData.success){
+        console.log(parsedData)
      res.json({
-        message:data.error.message
+        message:"request body is not valid"
     })
     return
     }
-    res.json({
-        message:"123"
+
+  try {
+    const user=await prismaClient.user.create({
+    data : {
+
+        email:parsedData.data?.username,
+        password:parsedData.data.password,
+        name:parsedData.data.name
+
+    }
+    
+
+   })
+   res.json({
+    userId:user.id
+   })
+}
+catch(e){
+    res.status(411).json({
+        message:"user already exists"
     })
+}
 
  })
  app.post("/signin",(req,res)=>{
